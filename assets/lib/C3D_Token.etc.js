4e956285390bc1ae979697cca91ff5897d2cf76e
Dean Connolly <31525346+dconnolly95@users.noreply.github.com>
2020-03-05 23:17:09 +1000

    Added more stuff
    DappDirect feed, and a clone of Sacrific3D

--- assets/lib/C3D_Token.etc.js
+++ assets/lib/C3D_Token.etc.js
@@ -42,8 +42,6 @@ if (masternode == null) {
     masternode = "0x0000000000000000000000000000000000000000";
 }
 
-new ClipboardJS('.button');
-
 function copyToClipboard (text) {
     if (window.clipboardData && window.clipboardData.setData) {
         // IE specific code path to prevent textarea being shown while dialog is visible.
